b714af2d4f890f5bb5503a93c2f5058c8735c2a0
Patrick Ong <p.ongsh@gmail.com>
2013-07-19 16:11:34 +0800

    web.js reads from index.html and prints out content

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf = fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
